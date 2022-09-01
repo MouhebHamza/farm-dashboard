dc288377406a415f70b8317b69dec11ac6f6e39a
MouhebHamza <mbmouheb@gmail.com>
2022-09-01 11:24:01 +0100

    final commit

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,23 +1,22 @@
 import React from "react"
-import ProductCard from "../../components/ProductCard/ProductCard"
 import RightSide from "../../components/RightSide/RightSide"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import "./Products.css"
+import ProductCard from "../../components/ProductCard/ProductCard"
+
 
 function Products ()
 {
     
     return (
-        
-            
         <>
-            <Sidebar  />
+            <Sidebar />
             <div className='products-container'>
-            <h1>Our products</h1>
-            <div className='stats-container'>
-                <ProductCard />
+                <h1>Our products</h1>
+                <div className='stats-container'>
+                    <ProductCard />
                 </div>
-                </div>
+            </div>
             <RightSide />
         </>
     )
